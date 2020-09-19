ad2de321749b68aceb978a499b81f7efd1ad9cac
loganmcbride148 <loganmcbride148@gmail.com>
2020-09-19 06:38:12 -0700

    Add UI for todo-list with dummy data. Also modify REAME to say who is on the team

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-
+    <List />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import List from "@/components/List";
 export default {
   name: 'Home',
   components: {
+    List,
   }
 }
 </script>
